059cdbf29da40855ab31fb14450b03ac336f1006
Emin Akyüz <emn.ak100@gmail.com>
2023-07-07 17:35:42 +0300

    EA-Commit 1

---
+++ TraversalCoreProje/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLayer.EntityFramework;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
